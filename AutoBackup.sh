348a78613239491c602bb7f6c8e6007a495cec13
Rudolf Suguitan <C13460538@mydit.ie>
2016-04-23 18:26:34 +0100

    Finished

---
+++ AutoBackup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Declaring variables
+EXCLUDE_DIRS="/* --exclude=/dev/* --exclude=/boot/* --exclude=/cdrom/* --exclude=/dev/* --exclude=/home/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/lib/* --exclude=/mnt/* --exclude=/media/* --exclude=/lost+found/* --exclude=/BackupFolder/*  --exclude=/RestoreFolder/* --exclude=/ZippedFiles/* --exclude=/usr/* --exclude=/var/*"
+DATE=`date +%d%m%y` #-%X`
+DATE2=`date +"%A %d-%B-%Y %R"`
+FILENAME=Backup-$DATE.tar.gz
+
+SBLOG="--> $DATE2 -- $FILENAME -- Full System Back Up -- SUCCESSFULL <--"
+FBLOG="--> $DATE2 -- $FILENAME -- Full System Back Up -- UNSUCCESSFULL <--"
+cd /
+mkdir -p BackupFolder
+mkdir -p ZippedFiles
+BACKUP_TO="/BackupFolder/"
+
+tar cvpfz /ZippedFiles/$FILENAME $EXCLUDE_DIRS && rsync -av /ZippedFiles/ $BACKUP_TO
+
+if [ "$?" -eq "0" ]
+then
+	if [ ! -e /$BACKUP_TO/LogFile.txt ]
+	then
+   		echo $SBLOG > /$BACKUP_TO/LogFile.txt
+	else
+   		echo $SBLOG >> /$BACKUP_TO/LogFile.txt
+	fi
+
+else
+	if [ ! -e /$BACKUP_TO/LogFile.txt ]
+        then
+                echo $FBLOG > /$BACKUP_TO/LogFile.txt
+        else
+                echo $FBLOG >> /$BACKUP_TO/LogFile.txt
+        fi
+fi
+
+
