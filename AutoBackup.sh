b08a88112f52ab9f544995c456503608cf010929
RudolfSuguitan <C13460538@mydit.ie>
2016-04-26 00:38:03 +0100

    updates

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #Declaring variables
 DIR_EX="/* --exclude=/dev/* --exclude=/boot/* --exclude=/cdrom/* --exclude=/dev/* --exclude=/home/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/lib/* --exclude=/mnt/* --exclude=/media/* --exclude=/lost+found/* --exclude=/BackupFolder/*  --exclude=/RestoreFolder/* --exclude=/ZippedFiles/* --exclude=/usr/* --exclude=/var/*"
 DATE=`date +%d%m%y` #-%X`
@@ -7,10 +8,25 @@ FILENAME=LinuxBackup-$DATE.tar.gz
 
 SBLOG="--> $DATE2 -- $FILENAME -- Full System Back Up -- SUCCESSFULL <--"
 FBLOG="--> $DATE2 -- $FILENAME -- Full System Back Up -- UNSUCCESSFULL <--"
+FBLOG2="--> $DATE2 -- $FILENAME -- Full System Back Up -- UNSUCCESSFULL - Must be Root <--"
+
 cd /
 mkdir -p BackupFolder
 mkdir -p ZippedFiles
 BACKUP_TO="/BackupFolder/"
+
+#forcing the user to run as root
+if [ "$EUID" -ne "0" ]
+then
+	if [ ! -e /$BACKUP_TO/LogFile.txt ]
+	then
+		echo $FBLOG2 > /$BACKUP_TO/LogFile.txt
+	else
+		echo $FBLOG2 >> /$BACKUP_TO/LogFile.txt
+	fi
+
+        exit
+fi
 
 tar cvpfz /ZippedFiles/$FILENAME $DIR_EX && rsync -av /ZippedFiles/ $BACKUP_TO
 
